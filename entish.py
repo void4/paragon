b8909c960374a5ea5a4b403109c6689bccdafaab
void4 <void4@users.noreply.github.com>
2017-10-24 13:11:56 +0200

    Clean output, add refuel instruction

--- entish.py
+++ entish.py
@@ -1,4 +1,7 @@
-class AD(dict):
+from collections import OrderedDict
+import os
+
+class AD(OrderedDict):
 	__getattr__ = dict.__getitem__
 	__setattr__ = dict.__setitem__
 	__delattr__ = dict.__delitem__
@@ -8,41 +11,58 @@ def pretty(d, indent=0):
 		print('\t' * indent + str(key), end="")
 		if isinstance(value, dict):
 			pretty(value, indent+1)
-		elif isinstance(value, list) and all([isinstance(v, dict) for v in value]):
+		elif isinstance(value, list):# and all([isinstance(v, dict) for v in value]):
 			print("")
 			for v in value:
-				pretty(v, indent+1)
+				if isinstance(v, dict):
+					pretty(v, indent+1)
+				else:
+					print("\t" * (indent+1) + str(v))
 		else:
 			print('\t' * (indent+1) + str(value))
 
 child = {
-	"steps": 10,
+	"steps": 0,
 	"index": 0,
 	"memory": ["ADD", "JUMP"],
 }
 
-program = {
-	"steps": 5,
+middle = {
+	"steps": 0,
 	"index": 0,
-	"memory": [AD(child)],
+	"memory": ["COPY", AD(child), "JUMP"],
+}
+
+program = {
+	"steps": 20,
+	"index": 0,
+	"memory": ["COPY", AD(child), "JUMP"],
 }
 
 
 def step(program):
-    program.steps -= 1
-    instr = program.memory[program.index]
-    if isinstance(instr, dict):
-        if instr["steps"] == 0:
-            program.index += 1
-        program.memory[program.index] = step(instr)
-    elif instr == "ADD":
-        program.memory.append(1)
-    elif instr == "JUMP":
-        program.index = 0
-    else:
-        print("Invalid instruction", instr)
+	program.steps -= 1
+	instr = program.memory[program.index]
+	print("INSTR", instr)
+	if isinstance(instr, dict):
+		if instr["steps"] == 0:
+			program.index += 1
+		else:
+			program.memory[program.index] = step(instr)
+	elif instr == "ADD":
+		program.memory.append(program.steps)
+		program.index += 1
+	elif instr == "JUMP":
+		program.index = 0
+	elif instr == "COPY":
+		nextcell = program.memory[program.index+1]
+		if isinstance(nextcell, dict):
+			nextcell.steps = min(program.steps, 5)
+		program.index += 1
+	else:
+		print("Invalid instruction", instr)
 
-    return program
+	return program
 
 program = AD(program)
 pretty(program)
@@ -53,4 +73,7 @@ while True:
 	print("ITER %i\n" % iterations)
 	pretty(program)
 	if program["steps"] == 0:
+		print("Exiting main (OutOfGas).")
 		break
+	input()
+	os.system("clear")
